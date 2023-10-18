14efc59ddb1f454eb1bdf61c55a54ab5fea1b2a9
ihumire <ignacio.humire@ug.uchile.cl>
2023-10-18 09:23:15 -0300

    Test&Fix Chicken

--- src/test/scala/model/units/ChickenTest.scala
+++ src/test/scala/model/units/ChickenTest.scala
@@ -10,9 +10,11 @@ class ChickenTest extends munit.FunSuite{
   private val defense = -1
   private val evasion = 1
   private var chicken: Chicken = _
+  private var chicken1: Chicken = _
 
   override def beforeEach(context: BeforeEach): Unit = {
     chicken = new Chicken()
+    chicken1 = new Chicken()
   }
 
   test("A chicken should have correctly set their attributes") {
@@ -69,4 +71,71 @@ class ChickenTest extends munit.FunSuite{
     chicken.decreaseHp(valueToDecrease)
     assertEquals(chicken.currHp, expectedHp)
   }
+
+  test("A chicken should be able to increase his currHp, without overflow maxHp") {
+    val valueToIncrease: Int = 1
+    val expectedHp: Int = chicken.currHp + valueToIncrease
+    chicken.increaseHp(valueToIncrease)
+    assertNotEquals(chicken.currHp, expectedHp)
+  }
+
+  test("A chicken should be able to increase his currHp, doing a normal addition") {
+    chicken.decreaseHp(2)
+    val valueToIncrease: Int = 1
+    val expectedHp: Int = chicken.currHp + valueToIncrease
+    chicken.increaseHp(valueToIncrease)
+    assertEquals(chicken.currHp, expectedHp)
+  }
+
+  test("A chicken should be able to not being killed when his currHp is not 0") {
+    val expected: Boolean = true
+    chicken.killWildUnit()
+    assertNotEquals(chicken.death, expected)
+  }
+
+  test("A chicken should be able to death when his currHp is 0") {
+    chicken.decreaseHp(3)
+    val expected: Boolean = true
+    chicken.killWildUnit()
+    assertEquals(chicken.death, expected)
+  }
+
+  test("A chicken should be able to give his final number of attack for the duel") {
+    for (x <- 1 to 6) {
+      val value: Int = x
+      val expectedValue: Int = value + chicken.attack
+      val finalAttack: Int = chicken.attackCharacter(value)
+      assertEquals(finalAttack, expectedValue)
+    }
+  }
+
+  test("A chicken should be able to give his final number damage at the duel") {
+    for (x <- 1 to 6){
+      val value: Int = x
+      // chicken will attack
+      val finalAttack: Int = chicken.attackCharacter(value)
+      for (y <- 1 to 6){
+        val value1: Int = y
+        // chicken1 will defend
+        val expectedValue: Int = math.max(1, finalAttack - (value1 + chicken1.defense))
+        val finalDefend: Int = chicken1.defendCharacter(finalAttack, value1)
+        assertEquals(finalDefend, expectedValue)
+      }
+    }
+  }
+
+  test("A chicken should be able to give his final number damage at the duel") {
+    for (x <- 1 to 6) {
+      val value: Int = x
+      // chicken will attack
+      val finalAttack: Int = chicken.attackCharacter(value)
+      for (y <- 1 to 6){
+        val value1: Int = y
+        // chicken 1 will defend
+        val finalAvoid: Int = chicken1.avoidCharacter(finalAttack, value1)
+        assertEquals(finalAvoid,finalAttack)
+      }
+    }
+  }
 }
+                              
