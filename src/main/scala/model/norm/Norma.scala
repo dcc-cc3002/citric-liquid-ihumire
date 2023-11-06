6b310306faeec889b787d46daab7229bad78e6b8
ihumire <ignacio.humire@ug.uchile.cl>
2023-11-06 01:58:03 -0300

    Docs Norma,Norma1

--- src/main/scala/model/norm/Norma.scala
+++ src/main/scala/model/norm/Norma.scala
@@ -4,17 +4,51 @@ package model.norm
 import cl.uchile.dcc.citric.model.roads.{AllRoads, Road, StarsRoad, VictoriesRoad}
 import cl.uchile.dcc.citric.model.units.PlayerCharacter
 /**
- * The `Norma` trait represents a set of conditions or goals for a player character in the game.
+ * The `Norma` trait represents a character's Norma level and defines methods to check and manipulate it.
  *
- * Player characters can pursue various normas with specific criteria, such as boosting stats, collecting stars,
- * achieving victories, or fulfilling all norma conditions.
+ * Norma is a character's personal victory condition in the game, and different actions can help them progress toward it.
+ * Norma can be related to various factors, such as boosts, stars, victories, or a combination of these.
  *
- * @see [[PlayerCharacter]]
- */
+ * @author [[https://github.com/ihumire Ignacio Humire S.]]
+ * */
 trait Norma {
+  /**
+   * Checks and updates the character's Norma level based on their current status and actions related to boosts.
+   *
+   * @param player The PlayerCharacter for whom to check and update the Norma level.
+   * @return The updated Norma level based on the character's actions related to boosts.
+   */
   def checkBoost(player: PlayerCharacter): Norma
+
+  /**
+   * Checks and updates the character's Norma level based on their current status and actions related to stars.
+   *
+   * @param player The PlayerCharacter for whom to check and update the Norma level.
+   * @return The updated Norma level based on the character's actions related to stars.
+   */
   def checkStars(player: PlayerCharacter): Norma
+
+  /**
+   * Checks and updates the character's Norma level based on their current status and actions related to victories.
+   *
+   * @param player The PlayerCharacter for whom to check and update the Norma level.
+   * @return The updated Norma level based on the character's actions related to victories.
+   */
   def checkVictories(player: PlayerCharacter): Norma
+
+  /**
+   * Checks and updates the character's Norma level based on their current status and a comprehensive evaluation.
+   *
+   * @param player The PlayerCharacter for whom to check and update the Norma level.
+   * @return The updated Norma level based on a comprehensive assessment of the character's status.
+   */
   def checkAll(player: PlayerCharacter): Norma
+
+  /**
+   * Returns the numeric value representing the character's current Norma level.
+   *
+   * @return The numeric value of the character's Norma level.
+   */
   def toNumber(): Int
 }
+
--- src/main/scala/model/norm/Norma1.scala
+++ src/main/scala/model/norm/Norma1.scala
@@ -3,20 +3,52 @@ package model.norm
 
 import model.units.PlayerCharacter
 
+/**
+ * The `Norma1` class represents a specific Norma level for a character in the game.
+ * This Norma level defines the initial character status and requirements to achieve a higher Norma level.
+ * Players need to fulfill certain criteria related to stars and victories to progress to Norma2.
+
+ * @author [[https://github.com/ihumire Ignacio Humire S.]]
+ */
 class Norma1 extends Norma {
+  /**
+   * Checks and updates the character's Norma level based on their current status and actions related to boosts and the selected road.
+   *
+   * @param player The PlayerCharacter for whom to check and update the Norma level.
+   * @return The updated Norma level based on the character's actions related to boosts and the selected road.
+   */
   def checkBoost(player: PlayerCharacter): Norma = player.currRoad.myRoad(this, player)
+
+  /**
+   * Checks and updates the character's Norma level based on their current status and actions related to stars.
+   *
+   * @param player The PlayerCharacter for whom to check and update the Norma level.
+   * @return The updated Norma level based on the character's actions related to stars.
+   */
   def checkStars(player: PlayerCharacter): Norma = {
     var norm: Norma = new Norma1
     if (player.currStars >= 10) norm = new Norma2
     norm
   }
 
+  /**
+   * Checks and updates the character's Norma level based on their current status and actions related to victories.
+   *
+   * @param player The PlayerCharacter for whom to check and update the Norma level.
+   * @return The updated Norma level based on the character's actions related to victories.
+   */
   def checkVictories(player: PlayerCharacter): Norma = {
     var norm: Norma = new Norma1
     if (player.currVictories >= 1) norm = new Norma2
     norm
   }
 
+  /**
+   * Checks and updates the character's Norma level based on a comprehensive evaluation of their current status.
+   *
+   * @param player The PlayerCharacter for whom to check and update the Norma level.
+   * @return The updated Norma level based on a comprehensive assessment of the character's status.
+   */
   def checkAll(player: PlayerCharacter): Norma = {
     var norm: Norma = new Norma1
     if (player.currStars >= 10) norm = new Norma2
@@ -24,5 +56,11 @@ class Norma1 extends Norma {
     norm
   }
 
+  /**
+   * Returns the numeric value representing the character's current Norma level, which is 1 for Norma1.
+   *
+   * @return The numeric value of the character's Norma level (1 for Norma1).
+   */
   def toNumber(): Int = 1
 }
+
