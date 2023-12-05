e6712847f10f14c741e1fae028726a7f139512b0
ihumire <ignacio.humire@ug.uchile.cl>
2023-12-05 16:15:35 -0300

    Fix AbsGameState Test GameController

--- src/main/scala/model/states/AbsGameState.scala
+++ src/main/scala/model/states/AbsGameState.scala
@@ -2,7 +2,11 @@ package cl.uchile.dcc.citric
 package model.states
 
 abstract class AbsGameState extends GameState{
-  protected var currChapter: Int = 1
+  private var _currChapter: Int = 1
+  def currChapter: Int = _currChapter
+  def currChapter_=(newCurrChapter: Int): Unit = {
+    _currChapter = newCurrChapter
+  }
   def newGame(): Unit = {}
   def newChapter(): Unit = {}
   def playTurn(): Unit = {}
--- src/test/scala/model/controller/GameControllerTest.scala
+++ src/test/scala/model/controller/GameControllerTest.scala
@@ -1,16 +1,33 @@
 package cl.uchile.dcc.citric
 package model.controller
 
-import model.states.{ChapterState, PreGameState}
+import model.states.{ChapterState, CombatState, EndState, MovePlayerState, PanelState, PlayerTurnState, PreGameState, RecoveryState, ResponseState}
 
 class GameControllerTest extends munit.FunSuite {
   private var controller: GameController = _
-
+  private var chapter: ChapterState = _
+  private var combat: CombatState = _
+  private var end: EndState = _
+  private var movePlayer: MovePlayerState = _
+  private var panel: PanelState = _
+  private var playerTurn: PlayerTurnState = _
+  private var preGame: PreGameState = _
+  private var recovery: RecoveryState = _
+  private var response: ResponseState = _
   override def beforeEach(context: BeforeEach): Unit = {
     controller = new GameController()
+    chapter = new ChapterState(controller)
+    combat = new CombatState(controller)
+    end = new EndState(controller)
+    movePlayer = new MovePlayerState(controller)
+    panel = new PanelState(controller)
+    playerTurn = new PlayerTurnState(controller)
+    preGame = new PreGameState(controller)
+    recovery = new RecoveryState(controller)
+    response = new ResponseState(controller)
   }
 
-  test("The Controller have an initial state"){
+  test("The controller have an initial state"){
     assert(controller.state.isInstanceOf[PreGameState])
   }
 
