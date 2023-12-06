28cf452d467d9a7a626c2ad10184242fcdb71c8b
ihumire <ignacio.humire@ug.uchile.cl>
2023-12-06 01:24:34 -0300

    Docs Subject, Observer

--- src/main/scala/model/observer/Observer.scala
+++ src/main/scala/model/observer/Observer.scala
@@ -3,6 +3,21 @@ package model.observer
 
 import model.observable.Subject
 
+/**
+ * A trait representing an observer in the observer pattern.
+ *
+ * Observers are entities that are notified by subjects about changes.
+ * The `update` method is called when the observed subject changes its state.
+ *
+ * @author [[https://github.com/ihumire Ignacio Humire S.]]
+ *
+ */
 trait Observer {
+  /**
+   * This method is called by the observed subject when its state changes.
+   *
+   * @param observable The subject that changed its state.
+   * @param value      The value associated with the change.
+   */
   def update(observable: Subject, value: Any): Unit
 }
